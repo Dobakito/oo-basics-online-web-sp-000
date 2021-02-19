891999615944b0076833f204dfda6def62e54899
Michael Dobak <mdobak44@icloud.com>
2021-02-19 21:05:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,14 @@
 class Book
   def initialize(title)
     @title = title
+    @author = author
+    @page_count = page_count
   end
   
-  attr_accessor
+  attr_accessor :title :author :page_count
   
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
 
