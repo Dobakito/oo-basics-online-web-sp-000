7b183d8e2bb08910e97a908c9988bac2548cb92c
Michael Dobak <mdobak44@icloud.com>
2021-02-19 21:43:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,9 @@ class Shoe
     @brand = brand
   end
   
-  def cobble
-  
+  def cobble(condition)
+    if condition == "old"
+      condition = "new"
+      puts "Your shoe is as good as new!"
   end
 end   
